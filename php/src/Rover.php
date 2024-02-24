ac6529145d4a1bc01c9a12aa579e9dcc4ec3e0d9
Fuen Sansano <fuensansano@gmail.com>
2024-02-24 18:30:29 +0100

    refactor: make more specific the command use
    Co-authored-by: Emmanuel Valverde Ramos <evrtrabajo@gmail.com>

--- php/src/Rover.php
+++ php/src/Rover.php
@@ -9,9 +9,11 @@ class Rover
         if ($command === 'MM') {
             return "0:2:N";
         }
-        if ($command) {
+
+        if ($command === 'M') {
             return "0:1:N";
         }
+        
         return '0:0:N';
     }
 }
