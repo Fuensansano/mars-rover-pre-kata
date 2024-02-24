7b20af1909ea2f0b3e23e9904fb27ea082c671f1
Fuen Sansano <fuensansano@gmail.com>
2024-02-24 19:01:58 +0100

    refactor tests code
    Co-authored-by: Emmanuel Valverde Ramos <evrtrabajo@gmail.com>

--- php/tests/RoverTest.php
+++ php/tests/RoverTest.php
@@ -3,11 +3,31 @@
 namespace KataTests;
 
 use Kata\Rover;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 class RoverTest extends TestCase
 {
+    public static function moveCommand()
+    {
+        yield 'given_an_M_command_the_rover_should_be_0_1_N' => ['M', '0:1:N'];
+        yield 'given_an_MM_command_the_rover_should_be_0_2_N' => ['MM', '0:2:N'];
+        yield 'given_an_MMM_command_the_rover_should_be_0_3_N' => ['MMM', '0:3:N'];
+    }
+
+    #[test]
+    #[DataProvider('moveCommand')]
+    public function given_a_move_command_the_rover_should_move(string $command, string $expectedPosition)
+    {
+        $rover = new Rover();
+
+        $finalPosition = $rover->execute($command);
+
+        self::assertEquals($expectedPosition, $finalPosition);
+    }
+
+
     #[test]
     public function given_not_a_command_the_rover_landing_position_should_be_0_0_N(): void
     {
@@ -16,35 +36,5 @@ class RoverTest extends TestCase
         $finalPosition = $rover->execute('');
 
         self::assertSame('0:0:N', $finalPosition);
-    }
-
-    #[test]
-    public function given_an_M_command_the_rover_should_be_0_1_N(): void
-    {
-        $rover = new Rover();
-
-        $finalPosition = $rover->execute('M');
-
-        self::assertSame('0:1:N', $finalPosition);
-    }
-
-    #[test]
-    public function given_an_MM_command_the_rover_should_be_0_2_N(): void
-    {
-        $rover = new Rover();
-
-        $finalPosition = $rover->execute('MM');
-
-        self::assertEquals('0:2:N', $finalPosition);
-    }
-
-    #[test]
-    public function given_an_MMM_command_the_rover_should_be_0_3_N(): void
-    {
-        $rover = new Rover();
-
-        $finalPosition = $rover->execute('MMM');
-
-        self::assertEquals('0:3:N', $finalPosition);
     }
 }
